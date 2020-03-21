203c7153ace09c41304744d787b85e7d57bc0e1e
ahmedgit380@gmail.com <ahmed.zaakan@gmail.com>
2020-03-21 14:51:16 +0300

    Inserting Post

--- admin/inc/functions.php
+++ admin/inc/functions.php
@@ -1,25 +1,38 @@
-<?php  
+<?php
 
 function get_categories() {
-	include "connect.php";
-	$sql = "SELECT * FROM categories";
-
-	try {
-		$result = $con->query($sql);
-		return $result;
-	}
-	catch(Exception $e) {
-		echo "Erorr: ". $e->getMessage();
-	}
+    include "connect.php";
+    $sql = "select * from categories";
+    try {
+        $result = $con->query($sql);
+        return $result;
+    }
+    catch(Exception $e) {
+        echo  "Error: " .$e->getMessage();
+    }
 }
 
 
+//Post Function
+
+function insert_post($datetime, $title, $content, $author,  $excerpt, $image, $category, $tags) {
+   $fields = array($datetime, $title, $content, $author, $excerpt, $image, $category, $tags);
+   include "connect.php";
+   $sql = "INSERT INTO posts (datetime, title, content, author, excerpt, image, category, tags) VALUES
+(?,?,?,?,?,?,?,?) ";
+
+   try{
+       $result = $con->prepare($sql);
+
+       for($i = 1; $i <= 8; $i++){
+           $result->bindValue($i, $fields[$i - 1], PDO::PARAM_STR);
+       }
+       return $result->execute();
+   }catch(Exception $e) {
+       echo "Error: ". $e->getMessage();
+       return false;
+   }
+}
+ ?>
 
 
-
-
-
-
-
-
-?>
--- admin/post.php
+++ admin/post.php
@@ -2,9 +2,81 @@
 include "inc/header.php";
 include "inc/navbar.php";
 include "inc/functions.php";
-
-
 ?>
+
+
+<?php
+
+if($_SERVER['REQUEST_METHOD']  == 'POST') {
+	if(isset($_POST['addpost'])) {
+
+
+		$title = filter_input(INPUT_POST, 'title' , FILTER_SANITIZE_STRING);
+		$content = filter_input(INPUT_POST, 'content' , FILTER_SANITIZE_STRING);
+		$category = filter_input(INPUT_POST, 'category' , FILTER_SANITIZE_STRING);
+		$excerpt = filter_input(INPUT_POST, 'excerpt' , FILTER_SANITIZE_STRING);
+		$tags = filter_input(INPUT_POST, 'tags' , FILTER_SANITIZE_STRING);
+
+        $author = "mohammed"; //Temprary Author until creating admins
+
+        date_default_timezone_set("Asia/Riyadh");
+        $datetime = date( 'M-D-Y h:m', time());
+
+           
+		$image = $_FILES['image'];
+
+
+		$img_name = $image['name'];
+		$img_tmp_name = $image['tmp_name'];
+		$img_size = $image['size'];
+
+
+		$error_msg = "";
+		if(strlen($title) < 10 || strlen($title) > 200) {
+			$error_msg = "Title must be between 10 and 200";
+		}else if(strlen($content) < 100 || strlen($content) > 100000) {
+			$error_msg = "Content must be between 100 and 100000";
+		}else if(! empty($excerpt)){
+			If(strlen($excerpt) < 10 || strlen($excerpt) > 1000) {
+			$error_msg = "Excerpt must be between 10 and 1000";
+			}
+		}else {
+
+			if(! empty($img_name)) {
+				$img_extension = strtolower(explode('.', $img_name)[1]);
+
+				$allowed_extensions = array('jpg' , 'png' , 'jpeg');
+
+				If(! in_array($img_extension, $allowed_extensions)) {
+					$error_msg = "Allowed Extensions are jpg, png and jpeg ";
+				}else if ( $img_size > 9000000) {
+					$error_msg = "Image size must be less than 9M";
+				}
+			}
+		}
+
+        if(empty($error_msg)) {
+        	// Insert Date in Database
+        	if( insert_post($datetime, $title, $content, $author, $excerpt, $img_name, $category, $tags) ) {
+        		echo "Success";
+        	}
+        }
+	}
+}
+
+
+
+
+
+
+
+ ?>
+
+
+
+
+
+
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-sm-2">
@@ -13,7 +85,7 @@ include "inc/functions.php";
 		<div class="col-sm">
 			<div class="post">
 				<h3>Add new Post</h3>
-				<form action="post.php" method="POST" enctype="multipart/form-date">
+				<form action="post.php" method="POST" enctype="multipart/form-data">
 					<div class="form-group">
 						<input class="form-control" type="text" name="title" placeholder="Title" required autocomplete="off" >
 						<p class="error title-error">Title the must be between 10 and 200 characters</p>
